08db4dfe1f7c0ecaefaea5e6c0e5ef73bdf8fd5e
Jonathan Caron-Roberge <jonathan@sports-lamitis.com>
2021-08-24 07:54:46 -0400

    Add action scope to PUT url

--- src/routes.ts
+++ src/routes.ts
@@ -9,12 +9,12 @@ const router: Router = Router()
 router.get('/container/:userId/*', container.get)
 router.post('/container/:userId/*', container.save)
 router.delete('/container/:userId/*', container.destroy)
-router.put('/container/:userId/*', container.move)
+router.put('/container/move/:userId/*', container.move)
 
 router.get('/object/:userId/*', obj.get)
 router.post('/object/:userId/*', obj.upsert)
-router.put('/object/:userId/*', obj.move)
 router.delete('/object/:userId/*', obj.destroy)
+router.put('/object/move/:userId/*', obj.move)
 
 router.get('/metadata/:userId/*', obj.getMetadata)
 
